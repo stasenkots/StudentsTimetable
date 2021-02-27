f831666ff3956826230c0906f7d6121679e98c32
stasenkots <tstasenko2@gmail.com>
2021-02-27 22:05:58 +0300

    fix bug with dates

--- app/src/main/java/com/stasenkots/studentstimetable/ui/homework/dialog/HomeworkDatesViewModel.kt
+++ app/src/main/java/com/stasenkots/studentstimetable/ui/homework/dialog/HomeworkDatesViewModel.kt
@@ -5,16 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.stasenkots.logic.entity.lesson.Lessons
 import com.stasenkots.logic.utils.EVERY_WEEK_INT
+import com.stasenkots.logic.utils.convertToDayOfWeek
 import com.stasenkots.logic.utils.toLocalDate
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.temporal.TemporalAdjusters
-import java.time.temporal.WeekFields
-import java.util.*
 
 class HomeworkDatesViewModel : ViewModel() {
     private lateinit var items: List<LocalDate>
-    var checkedItem=0
+    var checkedItem = 0
     private val _day = MutableLiveData<LocalDate>()
     val day: LiveData<LocalDate>
         get() = _day
@@ -34,15 +33,19 @@ class HomeworkDatesViewModel : ViewModel() {
             it.dayOfWeek
         }
         val dates = mutableListOf<LocalDate>()
-        val currentDate=date.toLocalDate()
+        val currentDate = date.toLocalDate()
 
         for (dayOfWeek in daysOfWeek) {
             val period = if (dayOfWeek.week == EVERY_WEEK_INT) 1 else 2
-            val day=DayOfWeek.of(dayOfWeek.dayOfWeek)
-            var mDate=currentDate.with(TemporalAdjusters.nextOrSame(day))
+            val day = DayOfWeek.of(dayOfWeek.dayOfWeek)
+            var mDate = currentDate.with(TemporalAdjusters.nextOrSame(day))
+            if (mDate.convertToDayOfWeek().week != dayOfWeek.week) {
+                mDate = mDate.plusWeeks(1)
+            }
             for (i in 1..2) {
                 dates.add(mDate)
-                mDate=mDate.plusWeeks(period.toLong())
+                mDate = mDate.plusWeeks(period.toLong())
+
             }
         }
         return dates.sortedBy { it }
